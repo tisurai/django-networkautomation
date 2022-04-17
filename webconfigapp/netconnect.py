02f846a2730bcf5c64c247faa9dc450a0aa5fc64
tisurai <95120323+tisurai@users.noreply.github.com>
2022-04-18 01:22:04 +0300

    Add script to connect to router and return JSON

--- webconfigapp/netconnect.py
+++ webconfigapp/netconnect.py
@@ -1,22 +1,46 @@
+import environ
 import os
 from netmiko import ConnectHandler
 
-ssh_uname = 'cisco'
-ssh_pwd = 'strongsshpassword'
-connection = None
+class nodeconnect:
+    env = environ.Env()
+    env.read_env(env.str('ENV_PATH', '.env'))
 
-device = {
-    'device_type': 'cisco_ios',
-    'host': '192.168.100.1',
-    'username': ssh_uname,
-    'password': ssh_pwd
-}
+    def __init__(self, command):
+        self.command = command
+        self.username = self.env('SSH_USERNAME')
+        self.password = self.env('SSH_PASSWORD')
+        self.connection = None
+        self.data = {}
 
-connection = ConnectHandler(**device)
+    def get_connect(self):
+        device = {
+            'device_type': 'cisco_ios',
+            'host': '192.168.100.1',
+            'username': self.username,
+            'password': self.connection
+        }
 
-show_output = connection.send_command("show int descr")
-print(show_output)
+        self.connection = ConnectHandler(**device)
 
-cfg_list = ["int e0/2", "ip address 192.168.200.1 255.255.255.0", "description Connection to Marketing Switch", "no shut", "do copy run start"]
-cfg_output = connection.send_config_set(cfg_list)
-print(show_output)
+        show_output = connection.send_command(self.command, expect_string=r"#")
+        split_output = show_output.splitlines()
+        size = len(split_output)
+
+        for i in range(size):
+            if i > 1:
+                linestrip = split_output[i].strip()
+                break_line = linestrip.split()
+                self.data[break_line[0]] = {}
+                self.data[break_line[0]]['line'] = break_line[1]
+                self.data[break_line[0]]['protocol'] = break_line[2]
+                self.data[break_line[0]]['description'] = ' '.join(break_line[3:])
+                
+        return data
+
+
+    def send_config(self, conf_command):
+
+        cfg_list = ["int e0/2", "ip address 192.168.200.1 255.255.255.0", "description Connection to Marketing Switch", "no shut", "do copy run start"]
+        cfg_output = connection.send_config_set(cfg_list)
+        print(show_output)
--- webconfigapp/views.py
+++ webconfigapp/views.py
@@ -4,6 +4,14 @@ from webconfigapp.forms import ShowCommandForm
 
 def home(request):
     form = ShowCommandForm(request.POST or None)
+
+    if request.is_ajax():
+        if form.is_valid():
+            # Call the netconnect
+            show_cmd = request.POST.get('send_command')
+            router_object = nodeconnect(show_cmd)
+            return JsonResponse({'data':router_object})
+            
     context = {
         'form': form,
     }
